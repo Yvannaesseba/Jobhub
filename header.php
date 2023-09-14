bfee3edda2c0c76e99b95356b1bd3937551b1a59
Yvannaesseba <132270841+Yvannaesseba@users.noreply.github.com>
2023-09-14 20:43:38 +0100

    Add files via upload

---
+++ header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8"> 
+        <title>FLAPPY BOOK</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link href="csss/bootstrap.min.css" rel="stylesheet">
+        <link href="csss/bootstrap-glyphicons.css" type="text/css" rel="stylesheet">
+		<link href="csss/font-awesome.css" type="text/css" rel="stylesheet">
+		<link href="csss/my_style.css" type="text/css" rel="stylesheet">
+	</head>
+	<?php include('dbcon.php'); ?>
